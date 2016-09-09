7f31f51bce188c1d2c70f034c170b0da2951184b
Elisha <elisha.java@gmail.com>
2016-09-09 14:41:16 -0500

    added views and actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
 
 	def index
-		
+		# create view for this
+		@content_first = 'This is a sample post from an upcoming birthday'
+		@content_second = 'This is another post from another upcoming birthday'
 	end
 
 	def new
-		
+		# create view for this
 	end
 
 	def create
@@ -13,11 +15,11 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		
+		# create view for this
 	end
 
 	def show
-		
+		# create view for this
 	end
 
 	def destroy
